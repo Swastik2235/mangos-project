5d04244492bdc50060699b01ffd37e0afa10fae6
Swastik2235 <swastik2235a@gmail.com>
2026-01-25 12:47:22 +0530

    Add automatic superuser creation to build script

--- MangOS_backend/MangOS/build.sh
+++ MangOS_backend/MangOS/build.sh
@@ -8,3 +8,14 @@ python manage.py collectstatic --no-input
 
 # Try migrate with --fake-initial first, if that fails, try regular migrate
 python manage.py migrate --fake-initial || python manage.py migrate
+
+# Create superuser
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@test.com', 'admin123')
+    print('Superuser created!')
+else:
+    print('Superuser already exists!')
+"
